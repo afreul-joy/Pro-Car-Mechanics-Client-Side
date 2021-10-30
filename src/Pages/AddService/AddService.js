53cb0a3e34591f77a8d8bfe6d6c39f2ebbd0a8da
Afreul kaium Joy <afreuljoy@gmail.com>
2021-10-30 14:55:15 +0600

    form style added

--- src/Pages/AddService/AddService.js
+++ src/Pages/AddService/AddService.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import { useForm } from "react-hook-form";
+import './AddService.css'
 
 const AddService = () => {
+ const { register, handleSubmit } = useForm();
+  const onSubmit = data => console.log(data); 
   return (
-    <div>
-      <h3>Hello Add</h3>
+    <div className="add-service">
+      <h3>Please Add Service </h3>
+
+       <form onSubmit={handleSubmit(onSubmit)}>
+      <input {...register("name", { required: true, maxLength: 20 })} placeholder="Name" />
+      <textarea {...register("description")} placeholder="Description" />
+      <input type="number" {...register("price")}  placeholder="Price"/>
+      <input {...register("img")} placeholder="image url" />
+      <input type="submit" />
+      </form>
+       
     </div>
   );
 };
